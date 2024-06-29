b9ca186d52659e6866cfc20dee8bf06bcdf8cfb4
sklearn21 <sklearn21@gmail.com>
2024-06-29 19:41:31 +0530

    added supabase.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:blogger/core/theme/theme.dart';
 import 'package:blogger/features/presentation/pages/signin_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
