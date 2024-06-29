3ccb549408fdaf27e424a6806990a1fb4ecfdcf1
sklearn21 <sklearn21@gmail.com>
2024-06-29 22:49:59 +0530

    added auth remote interface and implementation,with exceptions.

---
+++ lib/features/data/data_sources/auth_remote_data_source.dart
@@ -0,0 +1,46 @@
+import 'package:blogger/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String password,
+    required String email,
+  });
+
+  Future<String> signInWithEmailPassword({
+    required String password,
+    required String email,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+  @override
+  Future<String> signInWithEmailPassword(
+      {required String password, required String email}) {
+    // TODO: implement signInWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword(
+      {required String name,
+      required String password,
+      required String email}) async {
+    try {
+      final response = await supabaseClient.auth
+          .signUp(password: password, email: email, data: {'name': name});
+
+      if (response.user == null) {
+        throw ServerExceptions(message: 'User is null');
+      }
+
+      return response.user!.id;
+    } catch (e) {
+      throw ServerExceptions(message: e.toString());
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:blogger/core/secrets/app_secrets.dart';
 import 'package:blogger/core/theme/theme.dart';
 import 'package:blogger/features/presentation/pages/signin_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+      url: AppSecrets.enterYourSupabaseUrl,
+      anonKey: AppSecrets.enterYourSupabaseAnonKey);
   runApp(const MyApp());
 }
 
