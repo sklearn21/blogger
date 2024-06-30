29bb7b8168f79516a1e6b4bc144d9bdce840cfdb
sklearn21 <sklearn21@gmail.com>
2024-06-30 09:26:14 +0530

    Created UserSignUp usecase.

---
+++ lib/features/domain/usecases/user_sign_up.dart
@@ -0,0 +1,24 @@
+import 'package:blogger/core/error/failures.dart';
+import 'package:blogger/core/usecase/usecase.dart';
+import 'package:blogger/features/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignUp implements Usecase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+
+  UserSignUp(this.authRepository);
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+        email: params.email, password: params.password, name: params.name);
+  }
+}
+
+class UserSignUpParams {
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignUpParams(
+      {required this.email, required this.password, required this.name});
+}
