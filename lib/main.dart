b67f279068aa6f5f98563a217c3d98d13f862a00
sklearn21 <sklearn21@gmail.com>
2024-06-30 11:29:02 +0530

    added multiblock provider to main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:blogger/core/secrets/app_secrets.dart';
 import 'package:blogger/core/theme/theme.dart';
+import 'package:blogger/features/data/data_sources/auth_remote_data_source.dart';
+import 'package:blogger/features/data/repositories/auth_repository_impl.dart';
+import 'package:blogger/features/domain/usecases/user_sign_up.dart';
+import 'package:blogger/features/presentation/bloc/auth_bloc.dart';
 import 'package:blogger/features/presentation/pages/signin_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -9,7 +14,15 @@ void main() async {
   final supabase = await Supabase.initialize(
       url: AppSecrets.enterYourSupabaseUrl,
       anonKey: AppSecrets.enterYourSupabaseAnonKey);
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+          create: (_) => AuthBloc(
+              userSignUp: UserSignUp(AuthRepositoryImpl(
+                  AuthRemoteDataSourceImpl(supabase.client)))))
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
